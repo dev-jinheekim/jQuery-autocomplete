e4999c6259c7cb16f23557747469d0d60aabb182
jay <mail@kimjinhee.me>
2018-09-10 22:35:26 +0900

    jQuery-Autocomplete 추가

--- app.js
+++ app.js
@@ -1,34 +1,18 @@
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
-const address = [
-    {"text": "경기도 고양시"},
-    {"text": "경기도 성남시"},
-    {"text": "경기도 하남시"}
-];
 
 app.use(logger('dev'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
-
-function filter(address, search) {
-    return address.filter(data => data.text.includes(search))
-}
+app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.get('/api/address', (req, res) => res.status(200).send({
-    message: address
-}));
+app.get('*', (req, res) => res.status(200).sendFile(path.join(__dirname, 'views/index.html')));
 
-app.get('/api/address/:search', (req, res) => res.status(200).send({
-    message: filter(address, req.params.search)
-}));
-
-app.get('*', (req, res) => res.status(200).send({
-    message: '주소 자동완성 prototyping'
-}));
 
 module.exports = app;
