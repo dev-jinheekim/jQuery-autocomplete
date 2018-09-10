60b0143bb9bcb7c3ccd062107e8a166b835c987c
jay <mail@kimjinhee.me>
2018-09-10 23:01:11 +0900

    json data 요청 추가

--- app.js
+++ app.js
@@ -11,6 +11,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
+const address = [
+    {value: "경기도 고양시", text: "경기도 고양시"},
+    {value: "경기도 고양이", text: "경기도 고양이"},
+    {value: "경기도 성남시", text: "경기도 성남시"},
+    {value: "경기도 하남시", text: "경기도 하남시"}
+];
+
+function filter(address, search) {
+    return address.filter(data => data.text.includes(search))
+}
+
+app.get('/api/address', (req, res) => res.status(200).send({
+    message: filter(address, req.query.search)
+}));
 
 app.get('*', (req, res) => res.status(200).sendFile(path.join(__dirname, 'views/index.html')));
 
