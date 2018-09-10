9d86cd2499d763edc0af84cbcfddbb47d9e71572
jay <mail@kimjinhee.me>
2018-09-10 20:09:54 +0900

    주소 데이터 추가, api 제작

--- app.js
+++ app.js
@@ -3,11 +3,29 @@ const logger = require('morgan');
 const bodyParser = require('body-parser');
 
 const app = express();
+const address = [
+    {"text": "경기도 고양시"},
+    {"text": "경기도 성남시"},
+    {"text": "경기도 하남시"}
+];
 
 app.use(logger('dev'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
+
+function filter(address, search) {
+    return address.filter(data => data.text.includes(search))
+}
+
+
+app.get('/api/address', (req, res) => res.status(200).send({
+    message: address
+}));
+
+app.get('/api/address/:search', (req, res) => res.status(200).send({
+    message: filter(address, req.params.search)
+}));
 
 app.get('*', (req, res) => res.status(200).send({
     message: '주소 자동완성 prototyping'
