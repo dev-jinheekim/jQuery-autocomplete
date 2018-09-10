c50e6e09270974458933743b251c5bcb90687307
jay <mail@kimjinhee.me>
2018-09-10 23:14:55 +0900

    adress data 파일로 제작

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const path = require('path');
+const data = require('./public/js/data');
 
 const app = express();
 
@@ -11,19 +12,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-const address = [
-    {value: "경기도 고양시", text: "경기도 고양시"},
-    {value: "경기도 고양이", text: "경기도 고양이"},
-    {value: "경기도 성남시", text: "경기도 성남시"},
-    {value: "경기도 하남시", text: "경기도 하남시"}
-];
-
 function filter(address, search) {
     return address.filter(data => data.text.includes(search))
 }
 
 app.get('/api/address', (req, res) => res.status(200).send({
-    message: filter(address, req.query.search)
+    message: filter(data.address, req.query.search)
 }));
 
 app.get('*', (req, res) => res.status(200).sendFile(path.join(__dirname, 'views/index.html')));
---
+++ public/js/data.js
@@ -0,0 +1,10 @@
+const address = [
+    {value: "경기도 고양시", text: "경기도 고양시"},
+    {value: "경기도 고양이", text: "경기도 고양이"},
+    {value: "경기도 성남시", text: "경기도 성남시"},
+    {value: "경기도 하남시", text: "경기도 하남시"}
+];
+
+module.exports = {
+    address,
+};
